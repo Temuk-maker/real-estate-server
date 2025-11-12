9ea6d28638ab5eaf016ae1374c447f877dc79dee
Temuk-maker <temuunbold.10@gmail.com>
2025-11-12 20:40:47 +0800

    Fix root static route

--- server.js
+++ server.js
@@ -92,3 +92,8 @@ app.get('/', (req, res) => {
 // =======================
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
